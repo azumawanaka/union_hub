4df4968809a8a767bae733290c6b32680ef4e5ed
Filjumar Jumamoy <filjumarjumamoy@Filjumars-MacBook-Air.local>
2023-11-13 16:09:37 +0800

    ongoing migrations

---
+++ database/migrations/2023_11_13_075952_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('service_type_id');
+            $table->string('title');
+            $table->integer('added_by');
+            $table->text('description')->nullable();
+            $table->text('preview_img')->nullable();
+
+            $table->unsignedBigInteger('client_id');
+            
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
---
+++ database/migrations/2023_11_13_080322_create_service_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
+            $table->float('budget')->default(0.00);
+            $table->string('duration');
+            $table->date('preferred_date');
+            $table->time('preferred_time');
+            $table->string('location')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->char('status')->default('pending');
+            $table->text('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_requests');
+    }
+};
