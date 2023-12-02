64347640fade0a63a78f09647ea863cc1bb670d5
filjumar <jumamoy.filjumar@gmail.com>
2023-12-03 00:35:02 +0800

    satisfying request UI

--- resources/views/includes/navheader.blade.php
+++ resources/views/includes/navheader.blade.php
@@ -2,7 +2,7 @@
     <div class="brand-logo">
         <a href="#">
             <b class="logo-abbr"><img src="{{ asset('assets/images/logo.png') }}" alt=""> </b>
-            <span class="logo-compact"><img src="{{ asset('assets/images/logo-compact.png') }}" alt=""></span>
+            <span class="logo-compact"><img src="{{ asset('assets/images/logo.png') }}" alt=""></span>
             <span class="brand-title">
                 <img src="{{ asset('assets/images/logo-text.png') }}" alt="">
             </span>
