21dbcec2df08565188b3024b17c150b5ee11093e
filjumar <jumamoy.filjumar@gmail.com>
2024-03-19 22:40:50 +0800

    added reports on admin ends

--- resources/views/pages/reports/index.blade.php
+++ resources/views/pages/reports/index.blade.php
@@ -10,24 +10,29 @@
                     <div class="row">
                         <div class="col-xl-4 col-md-4 col-sm-3 mb-4 mb-sm-0">
                             <div class="list-group" id="list-tab" role="tablist">
+
+                                @if (auth()->user()->role === 0)
                                 <a class="list-group-item list-group-item-action active d-flex justify-content-between"
                                     id="form-fields-list"
                                     data-toggle="list"
                                     href="#form-fields"
                                     role="tab"
                                     aria-controls="form-fields">Submit Report<i class="fa fa-paper-plane"></i></a>
+                                @endif
 
-                                @foreach ($reports as $report)
-                                    <a class="list-group-item list-group-item-action"
+                                @foreach ($reports as $key => $report)
+                                    <a class="list-group-item list-group-item-action pl-4 {{ auth()->user()->role === 1 && $loop->first ? ' active' : '' }}"
                                         id="list-{{ $report->id }}-list"
                                         data-toggle="list"
                                         href="#list-{{ $report->id }}"
                                         role="tab"
                                         aria-controls="{{ $report->id }}">
-                                        <i class="fa fa-circle text-{{ \App\Models\Report::STATUS[$report->status]['color'] }}"></i>
-                                        {{ \Illuminate\Support\Str::limit($report->description, 80) }}
+                                        <i class="fa fa-circle text-{{ \App\Models\Report::STATUS[$report->status]['color'] }} position-absolute circle-stat"></i>
+                                        {{ \Illuminate\Support\Str::limit($report->description, 80) }}<br/>
+                                        <small>{{ $report->reported_at }}</small>
                                     </a>
                                 @endforeach
+
                                 <div class="pagination mt-4">
                                     {{ $reports->links('vendor.pagination.bootstrap-4') }}
                                 </div>
@@ -36,20 +41,36 @@
                         <div class="col-xl-8 col-md-8 col-sm-9">
                             <div class="tab-content" id="nav-tabContent">
                                 @foreach ($reports as $report)
-                                    <div class="tab-pane fade" id="list-{{ $report->id }}">
-                                        @if ($report->status === 'pending' || $report->status === 'declined')
+                                    <div class="tab-pane fade{{ auth()->user()->role === 1 && $loop->first ? ' active show' : '' }}" id="list-{{ $report->id }}">
+                                        @if (auth()->user()->role === 0)
+                                            @if ($report->status === 'pending' || $report->status === 'declined')
+                                                <!-- Dropdown button -->
+                                                <div class="dropdown top-0 end-0 mt-2 text-right">
+                                                    <span class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        <i class="fa fa-cog"></i>
+                                                    </span>
+                                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                        <li><a class="dropdown-item" href="#" id="delete_report">Delete</a></li>
+                                                    </ul>
+                                                </div>
+                                            @endif
+                                        @else
                                             <!-- Dropdown button -->
                                             <div class="dropdown top-0 end-0 mt-2 text-right">
                                                 <span class="dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                                                     <i class="fa fa-cog"></i>
                                                 </span>
                                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                                    <li><a class="dropdown-item" href="#" id="delete_report">Delete</a></li>
-                                                    {{-- <li><a class="dropdown-item" href="#">Edit</a></li> --}}
+                                                    <li><a class="dropdown-item" href="#">Accept</a></li>
+                                                    <li><a class="dropdown-item" href="#">Decline</a></li>
+                                                    <li><a class="dropdown-item" href="#">Mark as Done</a></li>
                                                 </ul>
                                             </div>
                                         @endif
 
+                                        <div>
+                                            <i class="fa fa-user"></i> {{ $report->is_anonymous ? 'Anonymous' : $report->user->full_name }}
+                                        </div>
                                         <div class="d-flex py-4">
                                             <div class="col-md-8"><p>{!! $report->description !!}</p></div>
                                             <div class="col-md-4">
@@ -63,8 +84,10 @@
                                     </div>
                                 @endforeach
 
-                                {{-- form --}}
-                                @include('pages.reports.form.create')
+                                @if (auth()->user()->role === 0)
+                                    @include('pages.reports.form.create')
+                                @endif
+
                             </div>
                         </div>
                     </div>
@@ -113,6 +136,10 @@
                     $('#dummyIssuePicture').hide();
                     $('#issueImgPreview').hide();
                     $('#dummyIssuePicture').hide();
+
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 3000);
                 },
                 error: function(error) {
                     // error message here
